584e725ad542f75e7b703b156becc22fffd404a4
Paolo Insogna <paolo@cowtech.it>
2020-05-22 16:07:58 +0200

    feat: Initial commit.

---
+++ async-callback/after.js
@@ -0,0 +1,69 @@
+const got = require('got')
+const { createServer, request } = require('http')
+const { open, close, read } = require('fs').promises
+const { join } = require('path')
+const { format } = require('util')
+
+let unhandledRejections = 0
+let openedFds = 0
+
+setInterval(() => {
+  console.log(
+    format('MEMORY: %d, UNHANDLED REJECTIONS: %d, FD: %d', process.memoryUsage().rss, unhandledRejections, openedFds)
+  )
+}, 2000)
+
+process.on('unhandledRejection', () => unhandledRejections++)
+
+function handler(req, res, cb) {
+  let fh
+  open(join(__dirname, '../package.json'))
+    .then(file => {
+      fh = file
+      openedFds++
+      return fh.read(Buffer.alloc(100), 0, 100, 0)
+    })
+    .then(({ buffer, bytesRead: len }) => {
+      const key = JSON.parse(buffer.slice(0, len).toString('utf-8')).key
+      return got(`http://localhost:3001?key=${key}`, { retry: 0 })
+    })
+    .then(res => {
+      cb(null, res.body)
+    })
+    .catch(cb)
+    .finally(() => {
+      openedFds--
+      fh.close().catch(console.error)
+    })
+}
+
+const apiServer = createServer((req, res) => {
+  if (Math.random() < 0.1) {
+    res.writeHead(500, { 'Content-Type': 'text/plain' })
+    res.write('REJECTED')
+    res.end()
+    return
+  }
+
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.write('OK')
+  res.end()
+})
+
+const server = createServer((req, res) => {
+  handler(req, res, (err, body) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' })
+      res.write(err.message)
+      res.end()
+      return
+    }
+
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    res.write(body)
+    res.end()
+  })
+})
+
+apiServer.listen(3001)
+server.listen(3000)
---
+++ async-event-handlers/before.js
@@ -0,0 +1,95 @@
+const got = require('got')
+const { createServer, request } = require('http')
+const { createReadStream } = require('fs')
+const { join } = require('path')
+const { format } = require('util')
+
+let unhandledRejections = 0
+let openedFds = 0
+
+setInterval(() => {
+  console.log(
+    format('MEMORY: %d, UNHANDLED REJECTIONS: %d, FD: %d', process.memoryUsage().rss, unhandledRejections, openedFds)
+  )
+}, 2000)
+
+process.on('unhandledRejection', () => unhandledRejections++)
+
+function cbHandler(req, res, cb) {
+  open(join(__dirname, 'package.json'), (err, fd) => {
+    if (err) {
+      return cb(err)
+    }
+
+    read(fd, Buffer.alloc(100), 0, 100, 0, async (err, len, buffer) => {
+      if (err) {
+        return cb(err)
+      }
+
+      openedFds++
+
+      const key = JSON.parse(buffer.slice(0, len).toString('utf-8')).key
+      const res = await got(`http://localhost:3001?key=${key}`, { retry: 0 })
+
+      close(fd, err => {
+        if (err) {
+          return cb(err)
+        }
+
+        openedFds--
+        cb(null, res.body)
+      })
+    })
+  })
+}
+
+function streamHandler(req, res, cb) {
+  const stream = createReadStream(join(__dirname, '../package.json'))
+  openedFds++
+  let data = ''
+
+  stream.on('data', async chunk => {
+    stream.pause()
+    const res = await got(`http://localhost:3001?key=${chunk}`, { retry: 0 })
+    data += res.body
+    stream.resume()
+  })
+
+  stream.on('end', () => {
+    openedFds--
+    cb(null, data)
+  })
+}
+
+const apiServer = createServer((req, res) => {
+  if (Math.random() < 0.1) {
+    res.writeHead(500, { 'Content-Type': 'text/plain' })
+    res.write('REJECTED')
+    res.end()
+    return
+  }
+
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.write('OK')
+  res.end()
+})
+
+const server = createServer((req, res) => {
+  const handler = streamHandler // Math.random() < 0.5 ? cbHandler : streamHandler
+
+  handler(req, res, (err, body) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' })
+      res.write(err.message)
+      res.end()
+      return
+    }
+
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    res.write(body)
+    res.end()
+  })
+})
+
+apiServer.listen(3001)
+server.listen(3000)
