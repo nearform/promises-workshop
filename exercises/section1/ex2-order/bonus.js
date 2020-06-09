28678e2234487d8367e6755e0c96b061b897f732
James M Snell <jasnell@gmail.com>
2020-06-09 10:02:05 -0700

    Bonus round for order exercise

---
+++ exercises/section1/ex2-order/bonus.js
@@ -0,0 +1,17 @@
+const p = Promise.resolve()
+  .then(() => console.log('then 1'))
+  .finally(() => console.log('finally 1'))
+
+p.then(() => console.log('then 2'))
+ .finally(() => console.log('finally 2'))
+
+p.then(() => console.log('then 3'))
+ .finally(() => console.log('finally 3'))
+
+p.finally(() => console.log('finally 4'))
+
+;(async () => {
+  console.log('async 1')
+  await p
+  console.log('async 2')
+})()
