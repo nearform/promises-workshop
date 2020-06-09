df65b7224104ec0b7c67c97ca37d87a60cb34125
James M Snell <jasnell@gmail.com>
2020-06-09 11:49:09 -0700

    Caching exercise

---
+++ exercises/section2/caching-promises/README.md
@@ -0,0 +1,9 @@
+# Caching Promises
+
+Run the code in `broken.js` a few times. Why is the behavior inconsistent?
+
+```
+node broken
+```
+
+Why is the cache not being cleared correctly?
---
+++ exercises/section2/caching-promises/broken.js
@@ -0,0 +1,44 @@
+const { strictEqual } = require('assert')
+const { promisify } = require('util')
+const sleep = promisify(setTimeout)
+
+const cache = new Map()
+
+function handleError (err) {
+  console.log(err.message)
+}
+
+async function maybeSleep (n) {
+  const r = Math.random(10) * 10 | 0
+  if (r > 5) throw new Error('boom')
+  await sleep(n)
+}
+
+function doRequest (key, n) {
+  let p = cache.get(key)
+  if (!p) {
+    p = maybeSleep(n).then(() => cache.delete(key))
+    cache.set(key, p)
+  }
+  return p
+}
+
+doRequest('foo', 200)
+  .then(() => console.log('A'))
+  .catch(handleError)
+
+doRequest('foo', 100)
+  .then(() => console.log('B'))
+  .catch(handleError)
+
+doRequest('foo', 200)
+  .then(() => {
+    doRequest('foo', 50)
+      .then(() => console.log('C'))
+      .catch(handleError)
+  })
+  .catch(handleError)
+
+process.on('exit', () => {
+  strictEqual(cache.size, 0)
+})
