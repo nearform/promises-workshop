be0d016a6a894c94dd65dd32e731bfa8c75d49c5
Matteo Collina <hello@matteocollina.com>
2020-06-10 19:15:14 +0200

    http final

--- exercises/section3/http-server/final.js
+++ exercises/section3/http-server/final.js
@@ -11,16 +11,20 @@ const pipeline = promisify(_pipeline)
 const sleep = promisify(setTimeout)
 
 const server = createServer((request, response) => {
-  const file = createReadStream(__filename)
-  const hash = createHash('sha256')
-  pipeline(file, hash)
-    .then(() => sleep(1000))
-    .then(() => response.end(hash.digest().toString('hex').substr(0,10)))
+  hash(request, response)
     .catch((err) => {
       console.error(err.message)
       ressponse.statusCode = 500
       response.end(err.message)
     })
 });
+
+async function hash (request, response) {
+  const file = createReadStream(__filename)
+  const hash = createHash('sha256')
+  await pipeline(file, hash)
+  await sleep(1000)
+  response.end(hash.digest().toString('hex').substr(0,10))
+}
 
 server.listen(8000);
