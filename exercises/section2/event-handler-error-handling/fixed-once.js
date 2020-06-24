386e814d40f70a5d4671ccea18cbdc653e349928
Matteo Collina <hello@matteocollina.com>
2020-06-24 19:52:13 +0200

    Added fixed-once to ee examples

---
+++ exercises/section2/event-handler-error-handling/fixed-once.js
@@ -0,0 +1,38 @@
+const { EventEmitter, once } = require('events')
+const { promisify } = require('util')
+
+const foo = new EventEmitter()
+const sleep = promisify(setTimeout)
+
+// foo.on('something', async () => {
+//   await sleep(100)
+//   // You can catch and forward the error to the
+//   // event emitter yourself...
+//   try {
+//     functionThatDoesNotExist()
+//   } catch (err) {
+//     // Escape the Promise error handling trap!
+//     process.nextTick(() => foo.emit('error', err))
+//   }
+// })
+// 
+// foo.on('error', (err) => {
+//   throw new Error('boom')
+// })
+// 
+// process.on('uncaughtException', (err) => {
+//   console.log(err.message)
+// })
+// 
+// foo.emit('something')
+
+
+async function run () {
+  await once(foo, 'something')
+  await sleep(100)
+  functionThatDoesNotExist()
+}
+
+run().catch(console.log)
+
+foo.emit('something')
