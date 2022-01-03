2eb7ded10c117dbea308836fbf4b0a229bafb991
Jonas Galvez <jonasgalvez@gmail.com>
2022-01-03 09:24:48 -0300

    fix: lint

--- exercises/section1/puzzle/puzzle.mjs
+++ exercises/section1/puzzle/puzzle.mjs
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 // The challenge: This code prints a message to the console. Without
 // running the code, can you determine what it prints?
 //
@@ -13,69 +13,68 @@ import {
   parentPort
 } from 'worker_threads'
 
-const sleep = promisify(setTimeout);
+const sleep = promisify(setTimeout)
 
-async function bar(n, s, t) {
-  setImmediate(() => process.stdout.write(s));
-  await sleep(n);
-  return t;
+async function bar (n, s, t) {
+  setImmediate(() => process.stdout.write(s))
+  await sleep(n)
+  return t
 }
 
 process.on('unhandledRejection', (err) => {
-  process.stdout.write(err.message);
-});
+  process.stdout.write(err.message)
+})
 
 if (isMainThread) {
-  const lock = new Int32Array(new SharedArrayBuffer(4));
-  const worker = new Worker(new URL(import.meta.url), { workerData: lock });
-  worker.postMessage('S');
+  const lock = new Int32Array(new SharedArrayBuffer(4))
+  const worker = new Worker(new URL(import.meta.url), { workerData: lock })
+  worker.postMessage('S')
   worker.on('exit', async () => {
     try {
       const items = ['U', 'F']
-      const p = Promise.reject(items);
-      p.catch((items) => process.stdout.write(items[items.length-1]));
+      const p = Promise.reject(items)
+      p.catch((items) => process.stdout.write(items[items.length - 1]))
       p.then(() => process.stdout.write('P'))
-       .catch((items) => process.stdout.write(items.shift()));
-      throw new Error('D');
+        .catch((items) => process.stdout.write(items.shift()))
+      throw new Error('D')
     } finally {
-      process.stdout.write(' ');
-      throw new Error('N');
+      process.stdout.write(' ')
+      throw new Error('N')
     }
-  });
+  })
 
-  async function foo() {
-    process.stdout.write('O');
-    for (const m of await Promise.all([bar(20, 'R', 'M'), bar(10, 'O', 'I')]))
-      process.stdout.write(m)
+  async function foo () {
+    process.stdout.write('O')
+    for (const m of await Promise.all([bar(20, 'R', 'M'), bar(10, 'O', 'I')])) { process.stdout.write(m) }
   }
 
-  sleep(50).then(() => process.stdout.write('S'));
+  sleep(50).then(() => process.stdout.write('S'))
 
   new Promise((res) => {
-    process.stdout.write('B');
-    res('E');
+    process.stdout.write('B')
+    res('E')
   }).then((m) => process.stdout.write(m))
-    .finally(() => process.stdout.write(' '));
+    .finally(() => process.stdout.write(' '))
 
-  queueMicrotask(() => process.stdout.write('N'));
+  queueMicrotask(() => process.stdout.write('N'))
 
-  process.nextTick(() => process.stdout.write('K'));
+  process.nextTick(() => process.stdout.write('K'))
 
   setTimeout(() => {
-    process.stdout.write('E');
-    lock[0] = 1;
-    Atomics.notify(lock, 0, 1);
-  }, 100);
+    process.stdout.write('E')
+    lock[0] = 1
+    Atomics.notify(lock, 0, 1)
+  }, 100)
 
-  setImmediate(() => process.stdout.write('P'));
+  setImmediate(() => process.stdout.write('P'))
 
-  process.stdout.write('R');
+  process.stdout.write('R')
 
-  foo();
+  foo()
 } else {
   parentPort.on('message', (value) => {
-    Atomics.wait(workerData, 0, 0);
-    process.stdout.write(value);
-    parentPort.close();
-  });
+    Atomics.wait(workerData, 0, 0)
+    process.stdout.write(value)
+    parentPort.close()
+  })
 }
