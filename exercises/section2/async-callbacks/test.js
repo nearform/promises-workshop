b9287e8294eccb48f7f937258fb2d4a2084729c3
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2025-08-11 08:19:03 +0200

    chore: remove tap (#432)

--- exercises/section2/async-callbacks/test.js
+++ exercises/section2/async-callbacks/test.js
@@ -1,15 +1,15 @@
 const autocannon = require('autocannon')
 const got = require('got')
 const {resolve} = require('path')
-const t = require('tap')
+const {test, teardown} = require('node:test')
 
 require(resolve(process.cwd(), process.argv[2]))
 
-t.teardown(() => {
+teardown(() => {
   return got('http://localhost:3000', {method: 'DELETE', retry: 0})
 })
 
-t.test('It should get 200 or 500 as response code, with no timeouts', async t => {
+test('It should get 200 or 500 as response code, with no timeouts', async t => {
   t.plan(3)
 
   const result = await autocannon({
@@ -19,7 +19,7 @@ t.test('It should get 200 or 500 as response code, with no timeouts', async t =>
     timeout: 1,
   })
 
-  t.ok(result['2xx'] > 0, 'Received 200 responses')
-  t.ok(result['5xx'] > 0, 'Received 500 responses')
-  t.ok(result.timeouts === 0, 'All requests completed with no timeouts')
+  t.assert.ok(result['2xx'] > 0, 'Received 200 responses')
+  t.assert.ok(result['5xx'] > 0, 'Received 500 responses')
+  t.assert.ok(result.timeouts === 0, 'All requests completed with no timeouts')
 })
--- exercises/section2/convert-callbacks/test.js
+++ exercises/section2/convert-callbacks/test.js
@@ -1,12 +1,12 @@
 const { spawnSync } = require('child_process')
 const { resolve } = require('path')
-const t = require('tap')
+const {test} = require('node:test')
 
 const first = resolve(process.cwd(), 'before.js')
 const second = resolve(process.cwd(), process.argv[2])
 
-t.test('The output should be identical', async ({ same }) => {
+test('The output should be identical', async t => {
   const before = spawnSync(process.execPath, [first])
   const after = spawnSync(process.execPath, [second])
-  same(before.stdout, after.stdout)
+  t.assert.deepStrictEqual(before.stdout, after.stdout)
 })
